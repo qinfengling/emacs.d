038b8e02c13de827228c4e7e5aeeee92dd23628e
Mikeqin <Fengling.Qin@gmail.com>
2016-02-22 15:16:29 +0800

    Add open new line functions

--- init.el
+++ init.el
@@ -193,3 +193,27 @@
             (define-key org-trello-mode-map (kbd "C-c x l") 'org-trello-show-card-labels)
             (define-key org-trello-mode-map (kbd "C-c x u") 'org-trello-update-board-metadata)
             (define-key org-trello-mode-map (kbd "C-c x h") 'org-trello-help-describing-bindings)))
+
+;; Behave like vi's o command
+(defun open-next-line (arg)
+        "Move to the next line and then opens a line.
+    See also `newline-and-indent'."
+        (interactive "p")
+        (end-of-line)
+        (open-line arg)
+        (next-line 1)
+        (when newline-and-indent
+          (indent-according-to-mode)))
+
+;; Behave like vi's O command
+(defun open-previous-line (arg)
+        "Open a new line before the current one.
+     See also `newline-and-indent'."
+        (interactive "p")
+        (beginning-of-line)
+        (open-line arg)
+        (when newline-and-indent
+          (indent-according-to-mode)))
+
+(global-set-key (kbd "C-o") 'open-next-line)
+(global-set-key (kbd "C-M-o") 'open-previous-line)
